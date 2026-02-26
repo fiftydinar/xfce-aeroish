38590cd0cf19d0db031defb1a83ad0093fea7001
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-02-26 15:29:12 +0100

    Update gtk4-libadwaita-theming.sh

--- files/etc/profile.d/gtk4-libadwaita-theming.sh
+++ files/etc/profile.d/gtk4-libadwaita-theming.sh
@@ -2,5 +2,5 @@ if [ ! -e "${XDG_CONFIG_HOME:-$HOME/.config}/gtk-4.0/gtk.css" ]; then
   ln -s /usr/share/themes/ReVista/gtk-4.0/gtk.css "${XDG_CONFIG_HOME:-$HOME/.config}/gtk-4.0/gtk.css"
 fi
 if [ ! -e "${XDG_CONFIG_HOME:-$HOME/.config}/gtk-4.0/assets" ]; then
-  ln -s /usr/share/themes/ReVista/gtk-4.0/assets "${XDG_CONFIG_HOME:-$HOME/.config}/.config/gtk-4.0/assets"
+  ln -s /usr/share/themes/ReVista/gtk-4.0/assets "${XDG_CONFIG_HOME:-$HOME/.config}/gtk-4.0/assets"
 fi
