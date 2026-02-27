6a4e4875e49114f7247f847fc5743c1bc145bffa
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-02-27 22:05:59 +0100

    Set default compiz config and change ReVista name to Aeroish

--- files/etc/profile.d/gtk4-libadwaita-theming.sh
+++ files/etc/profile.d/gtk4-libadwaita-theming.sh
@@ -1,7 +1,7 @@
 mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}/gtk-4.0"
 if [ ! -e "${XDG_CONFIG_HOME:-$HOME/.config}/gtk-4.0/gtk.css" ]; then
-  ln -s /usr/share/themes/ReVista/gtk-4.0/gtk.css "${XDG_CONFIG_HOME:-$HOME/.config}/gtk-4.0/gtk.css"
+  ln -s /usr/share/themes/Aeroish/gtk-4.0/gtk.css "${XDG_CONFIG_HOME:-$HOME/.config}/gtk-4.0/gtk.css"
 fi
 if [ ! -e "${XDG_CONFIG_HOME:-$HOME/.config}/gtk-4.0/assets" ]; then
-  ln -s /usr/share/themes/ReVista/gtk-4.0/assets "${XDG_CONFIG_HOME:-$HOME/.config}/gtk-4.0/assets"
+  ln -s /usr/share/themes/Aeroish/gtk-4.0/assets "${XDG_CONFIG_HOME:-$HOME/.config}/gtk-4.0/assets"
 fi
