b7cb257b28b20cf78307e845d76b7cd09aeaba24
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-02-28 20:12:32 +0100

    Revert "Update fstab options for root filesystem"
    This reverts commit 73afaa7078d7e3bc7b697d22b56e07c2cf2a51c8.

--- files/usr/bin/remove-fstab-root.sh
+++ files/usr/bin/remove-fstab-root.sh
@@ -7,36 +7,7 @@ root_UUID=$(sed -n 's/.*root=UUID=\([^[:space:]]\{36\}\).*/UUID=\1/p' /proc/cmdl
 
 if [ -n "$root_UUID" ] && grep -E -q "^$root_UUID[[:space:]]/[[:space:]]" /etc/fstab && findmnt / | grep -q composefs && [ ! -L /boot/grub2/grub.cfg ]; then
     tmp=$(mktemp /tmp/fstab.XXXXXX) || exit 1
-
-    awk -v uuid="$root_UUID" '
-    BEGIN { OFS = "\t" }
-    $1 == uuid && $2 == "/" {
-        opts = $4
-        # if "ro" already present, leave line as-is
-        if (opts ~ /(^|,)ro(,|$)/) {
-            print
-            next
-        }
-        # replace rw occurrences with ro (handles several positions)
-        if (opts ~ /(^rw,|,rw,|,rw$|^rw$)/) {
-            sub(/^rw,/, "ro,", opts)
-            sub(/,rw,/, ",ro,", opts)
-            sub(/,rw$/, ",ro", opts)
-            sub(/^rw$/, "ro", opts)
-        } else {
-            if (opts == "") {
-                opts = "ro"
-            } else {
-                opts = opts "," "ro"
-            }
-        }
-        $4 = opts
-        print
-        next
-    }
-    { print }
-    ' /etc/fstab > "$tmp"
-
+    sed '/^'"$root_UUID"'[[:space:]]\/[[:space:]]/d' /etc/fstab > "$tmp"
     mv "$tmp" /etc/fstab
     rm -f "$tmp"
     touch "$stamp_file"
