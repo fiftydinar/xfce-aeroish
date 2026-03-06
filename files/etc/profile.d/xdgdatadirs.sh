6c73faa6a0f6bbae5a6a95af443681a579d3ff76
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-03-06 19:20:09 +0100

    Also add `/usr/local/share` to `XDG_DATA_DIRS`

--- files/etc/profile.d/xdgdatadirs.sh
+++ files/etc/profile.d/xdgdatadirs.sh
@@ -1,7 +1,18 @@
 XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+LOCAL_SHARE=/usr/local/share
 
+# /usr/local/share
 case ":${XDG_DATA_DIRS:-}:" in
-  *":${XDG_DATA_HOME}:"*) 
+  *":${LOCAL_SHARE}:"*)
+    ;;
+  *)
+    export XDG_DATA_DIRS="${XDG_DATA_DIRS:+${XDG_DATA_DIRS}:}${LOCAL_SHARE}"
+    ;;
+esac
+
+# XDG_DATA_HOME
+case ":${XDG_DATA_DIRS:-}:" in
+  *":${XDG_DATA_HOME}:"*)
     ;;
   *)
     export XDG_DATA_DIRS="${XDG_DATA_HOME}${XDG_DATA_DIRS:+:}${XDG_DATA_DIRS}"
