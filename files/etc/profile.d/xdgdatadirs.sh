e28616f94a7d1074edbcf69e46a32717002c7ad8
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-03-06 19:15:19 +0100

    Fix xdgdatadirs.sh profile script

--- files/etc/profile.d/xdgdatadirs.sh
+++ files/etc/profile.d/xdgdatadirs.sh
@@ -1,4 +1,9 @@
-case "$XDG_DATA_DIRS" in
-  *"${XDG_DATA_HOME:-$HOME/.local/share}:"*);;
-  *) export XDG_DATA_DIRS="${XDG_DATA_HOME:-$HOME/.local/share}:${XDG_DATA_DIRS}";;
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+case ":${XDG_DATA_DIRS:-}:" in
+  *":${XDG_DATA_HOME}:"*) 
+    ;;
+  *)
+    export XDG_DATA_DIRS="${XDG_DATA_HOME}${XDG_DATA_DIRS:+:}${XDG_DATA_DIRS}"
+    ;;
 esac
